0bb8ca86f18a429293cf533b0def3ec8ba2aba88
arms22 <arms22@6844b672-8a06-11de-b664-3b115b7b7a9b>
2010-03-04 12:01:47 +0000

    add license description

--- trunk/Prescaler/Prescaler.h
+++ trunk/Prescaler/Prescaler.h
@@ -1,3 +1,14 @@
+/*
+ Prescaler.h - Arduino Prescaler library
+ Copyright (c) arms22<arms22 at gmail.com>.  All right reserved.
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ 
+ Version 001 4 Mar 2010
+ */
 #ifndef Prescaler_h
 #define Prescaler_h
 
