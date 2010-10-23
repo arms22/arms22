66d7d3694960a1c0e686f5d93814276a1e6beb0c
arms22 <arms22@6844b672-8a06-11de-b664-3b115b7b7a9b>
2010-10-23 16:19:45 +0000

    add Dots library

---
+++ trunk/Dots/Dots.h
@@ -0,0 +1,49 @@
+/*
+  Dots.h - Arduino library for 8x8/5x7 LED Dot Matrix.
+  Copyright 2010 arms22. All right reserved.
+  
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+*/
+
+#ifndef DOTS_H
+#define DOTS_H
+
+#include <inttypes.h>
+#include <avr/pgmspace.h>
+
+class Dots
+{
+private:
+	unsigned long _lastUpdateTime;
+	uint16_t _updateInterval;
+	uint8_t _rowPins[8];
+	uint8_t _colPins[8];
+	uint8_t _buffer[8];
+	uint8_t _numOfRows;
+	uint8_t _numOfCols;
+	uint8_t _row;
+public:
+	Dots(uint8_t r0,uint8_t r1,uint8_t r2,uint8_t r3,
+		 uint8_t r4,uint8_t r5,uint8_t r6,uint8_t r7,
+		 uint8_t c0,uint8_t c1,uint8_t c2,uint8_t c3,
+		 uint8_t c4,uint8_t c5,uint8_t c6,uint8_t c7);
+	
+	Dots(uint8_t r0,uint8_t r1,uint8_t r2,uint8_t r3,
+		 uint8_t r4,uint8_t r5,uint8_t r6,
+		 uint8_t c0,uint8_t c1,uint8_t c2,uint8_t c3,
+		 uint8_t c4);
+	
+	void begin(void);
+	void write(uint8_t x, uint8_t y, uint8_t value);
+	void write(uint8_t y, uint8_t value);
+	void write(uint8_t y, const uint8_t values[], uint8_t size);
+	void clear(void);
+	void turnOff(void);
+	void turnOn(void);
+	void updateRow(void);
+	bool update(uint16_t time = 0);
+};
+
+#endif	// Dots.h
---
+++ trunk/Dots/examples/wave_animation/wave_animation.pde
@@ -0,0 +1,56 @@
+/*** Wave Animation
+ * by arms22 <http://arms22.blog91.fc2.com/>
+ * 
+ * Demonstrates the use of the Dots library
+ * 
+ * Matrix  Arduino
+ * 1  R5 -> 17(analog 3)
+ * 2  R7 -> 16(analog 2)
+ * 3  C2 -> 15(analog 1)
+ * 4  C3 -> 14(analog 0)
+ * 5  R8 -> 13
+ * 6  C5 -> 12
+ * 7  R6 -> 11
+ * 8  R3 -> 10
+ * 9  R1 -> 9
+ * 10 C4 -> 8
+ * 11 C6 -> 7
+ * 12 R4 -> 6
+ * 13 C1 -> 5
+ * 14 R2 -> 4
+ * 15 C7 -> 3
+ * 16 C8 -> 2
+ */
+
+#include <Dots.h>
+
+Dots myDots = Dots(9,4,10,6,17,11,16,13,5,15,14,8,12,7,3,2);
+
+const byte wave[] = {
+  B00110000,
+  B00001100,
+  B00000010,
+  B00000010,
+  B00001100,
+  B00110000,
+  B01000000,
+  B01000000,
+};
+
+void setup()
+{
+  myDots.begin();
+}
+
+int x = 0;
+
+void loop()
+{
+  int len = 8 - x;
+  myDots.write(x, wave, len);
+  myDots.write(0, wave+len, x);
+  x++;
+  if(x == 8) x = 0;
+  myDots.update(8);
+}
+
