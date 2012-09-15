84abbb5a33675cfe24e06be04044a91407d17e2d
arms22@gmail.com <arms22@gmail.com@6844b672-8a06-11de-b664-3b115b7b7a9b>
2012-09-15 06:54:54 +0000

    fix write() bug.

--- trunk/SoftModem/SoftModem.h
+++ trunk/SoftModem/SoftModem.h
@@ -1,51 +1,34 @@
 #ifndef SoftModem_h
 #define SoftModem_h
 
-#include <Stream.h>
-#include <inttypes.h>
+#include <Arduino.h>
 
 //#define SOFT_MODEM_BAUD_RATE   (126)
 //#define SOFT_MODEM_LOW_FREQ    (882)
 //#define SOFT_MODEM_HIGH_FREQ   (1764)
-//#define SOFT_MODEM_MAX_RX_BUFF (4)
+//#define SOFT_MODEM_RX_BUF_SIZE (4)
 
-//#define SOFT_MODEM_BAUD_RATE   (100)
-//#define SOFT_MODEM_LOW_FREQ    (800)
-//#define SOFT_MODEM_HIGH_FREQ   (1600)
-//#define SOFT_MODEM_MAX_RX_BUFF (4)
+//#define SOFT_MODEM_BAUD_RATE   (315)
+//#define SOFT_MODEM_LOW_FREQ    (1575)
+//#define SOFT_MODEM_HIGH_FREQ   (3150)
+//#define SOFT_MODEM_RX_BUF_SIZE (8)
 
-// #define SOFT_MODEM_BAUD_RATE   (315)
-// #define SOFT_MODEM_LOW_FREQ    (1575)
-// #define SOFT_MODEM_HIGH_FREQ   (3150)
-// #define SOFT_MODEM_MAX_RX_BUFF (8)
-
-// #define SOFT_MODEM_BAUD_RATE   (630)
-// #define SOFT_MODEM_LOW_FREQ    (3150)
-// #define SOFT_MODEM_HIGH_FREQ   (6300)
-// #define SOFT_MODEM_MAX_RX_BUFF (16)
-
-//#define SOFT_MODEM_BAUD_RATE   (600)
-//#define SOFT_MODEM_LOW_FREQ    (2666)
-//#define SOFT_MODEM_HIGH_FREQ   (4000)
-//#define SOFT_MODEM_MAX_RX_BUFF (16)
+//#define SOFT_MODEM_BAUD_RATE   (630)
+//#define SOFT_MODEM_LOW_FREQ    (3150)
+//#define SOFT_MODEM_HIGH_FREQ   (6300)
+//#define SOFT_MODEM_RX_BUF_SIZE (16)
 
 #define SOFT_MODEM_BAUD_RATE   (1225)
 #define SOFT_MODEM_LOW_FREQ    (4900)
 #define SOFT_MODEM_HIGH_FREQ   (7350)
-#define SOFT_MODEM_MAX_RX_BUFF (32)
+#define SOFT_MODEM_RX_BUF_SIZE (32)
 
 //#define SOFT_MODEM_BAUD_RATE   (2450)
-//#define SOFT_MODEM_LOW_FREQ    (7350)
-//#define SOFT_MODEM_HIGH_FREQ   (14700)
-//#define SOFT_MODEM_MAX_RX_BUFF (32)
+//#define SOFT_MODEM_LOW_FREQ    (4900)
+//#define SOFT_MODEM_HIGH_FREQ   (7350)
+//#define SOFT_MODEM_RX_BUF_SIZE (32)
 
-//  Brief carrier tone before each transmission
-//  1 start bit (LOW)
-//  8 data bits, LSB first
-//  1 stop bit (HIGH)
-//  1 push bit (HIGH)
-
-#define SOFT_MODEM_DEBUG       (0)
+#define SOFT_MODEM_DEBUG_ENABLE  (0)
 
 class SoftModem : public Stream
 {
@@ -58,9 +41,10 @@ private:
 	uint8_t _recvBits;
 	uint8_t _recvBufferHead;
 	uint8_t _recvBufferTail;
-	uint8_t _recvBuffer[SOFT_MODEM_MAX_RX_BUFF];
+	uint8_t _recvBuffer[SOFT_MODEM_RX_BUF_SIZE];
 	uint8_t _lowCount;
 	uint8_t _highCount;
+    unsigned long _lastWriteTime;
 	void modulate(uint8_t b);
 public:
 	SoftModem();
@@ -71,18 +55,11 @@ public:
 	virtual int read();
 	virtual void flush();
 	virtual int peek();
-	virtual void write(uint8_t data);
+    virtual size_t write(const uint8_t *buffer, size_t size);
+	virtual size_t write(uint8_t data);
 	void demodulate(void);
 	void recv(void);
 	static SoftModem *activeObject;
-#if SOFT_MODEM_DEBUG
-	void handleAnalogComp(bool high);
-	void demodulateTest(void);
-	uint8_t _errs;
-	uint16_t _ints;
-	uint8_t errs;
-	uint16_t ints;
-#endif
 };
 
 #endif
